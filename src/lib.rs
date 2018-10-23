d3cfff9468ebb86f2ad4d6089700c6f3c6b214b9
hazzard <enzo@turismocity.com>
2018-10-23 18:40:20 -0300

    feat:

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,17 @@
+extern {
+    fn appendNumberToBody(x: u32);
+    fn alert(x: u32);
+}
+
 #[no_mangle]
 pub extern fn add_one(x: u32) -> u32 {
     x + 1
+}
+
+#[no_mangle]
+pub extern fn run() {
+    unsafe {
+        appendNumberToBody(42);
+        alert(4);
+    }
 }
